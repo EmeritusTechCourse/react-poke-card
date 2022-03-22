82df6b5484fe9b784173e47b97cd92f64611b561
uvzoome <uvzumaya@gmail.com>
2022-03-22 12:03:56 -0600

    Change picture function added

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,32 +1,37 @@
-import React, { useContext } from "react";
-import {pokeDataContext} from "../App.js"
-import '../styles/PokemonCard.css'
+import React, { useContext, useState, useEffect } from "react";
+import {pokeDataContext} from "../App.js";
+import '../styles/PokemonCard.css';
 
 const PokemonCard = () => {
   const pokeInformation = useContext(pokeDataContext)
   const inputStyle = {}
+  let currentPokemonAbilities = [];
+  let currentPokemonMoves = [];
 
-  // if (pokeInformation.types[0].type.name === 'grass') {
-    
+  const [displayImage, setDisplayImage] = useState(pokeInformation.sprites.front_default)
+  const [displayedDetails, setDisplayedDetails] = useState(currentPokemonAbilities)
 
-  // }
-
-
+  useEffect(() => {
+      currentPokemonAbilities = pokeInformation.abilities.map(typeObj => typeObj.ability.name)
+      currentPokemonMoves = [pokeInformation.moves[0].move.name, pokeInformation.moves[1].move.name]
+    }, [])
 
   
-  
-
-  // if (value == "411") 
-  // $(".jp-front").css({backgroundImage:"url(http://www.lowestrates.ca/newcontent/img/creditcards/Gold_Rewards_Card_chip_467x293.png)"});
-
-  return(
-  <div className="card">
-      <img className="sprite" src={pokeInformation.sprites.front_default} alt={pokeInformation.name}></img>
+  return (
+  <div className="card" onClick={() => 
+    {
+      setDisplayImage(displayImage === pokeInformation.sprites.front_default ? pokeInformation.sprites.front_shiny : pokeInformation.sprites.front_default)
+      setDisplayedDetails(displayedDetails === currentPokemonAbilities ? currentPokemonMoves : currentPokemonAbilities)
+    }
+  }>
+    <div className="name">
+      <h4><b>{pokeInformation.name.charAt(0).toUpperCase() + pokeInformation.name.slice(1)}</b></h4>
+    </div>
+    <img className="sprite" src={displayImage} alt={pokeInformation.name}></img>
     <div className="container">
-      <h4><b>{pokeInformation.name}</b></h4>
-      <div>Types:</div>
+      <div>Abilities:</div>
       <ul>
-        {pokeInformation.types.length === 1 ? <li>{pokeInformation.types[0].type.name}</li>  : pokeInformation.types.map(typeObj => <li>{typeObj.type.name}</li>)}
+        {displayedDetails.map(detail => <li key={detail}>{detail}</li>)}
       </ul> 
     </div>
   </div>
